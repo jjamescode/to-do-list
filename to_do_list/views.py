84d79130a15925ccfc0d941322a94378d2ad0f35
jjamescode <jalandajames@yahoo.com>
2020-08-02 11:38:39 -0500

    added database and bootstrap

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import List
 
-# Create your views here.
+def home(request):
+    all_items = List.objects.all
+    return render(request, 'home.html', {'all_items': all_items})
+
+def about(request):
+    context = {'first_name': 'John', 'last_name': 'Girl'}
+    return render(request, 'about.html', context)
